94f2dab4391cd03a9d45b227f52563cb467ec37f
jsiewierski11 <jarid.siewierski@gmail.com>
2020-09-24 08:39:26 -0600

    Initial commit with vocab example.

---
+++ src/simple.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass, asdict
+import json, yaml
+import argparse
+
+
+@dataclass
+class TextGenerationConfig:
+    model_type: str
+    model_name_or_path: str
+    cached_dir: str
+    max_length: int
+    temperature: float
+    top_k: int
+    top_p: int
+    stop_token: str
+    do_sample: bool
+
+    @classmethod
+    def from_json(cls, json_file):
+        config_json = json.load(open(json_file))
+        return cls(**config_json)
+
+    @classmethod
+    def from_yaml(cls, yaml_file):
+        config_yaml = yaml.load(open(yaml_file), yaml.FullLoader)
+        return cls(**config_yaml)
+
+    def __iter__(self):
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
+
+
+
+if __name__ == "__main__":
+    # simple_config = TextGenerationConfig.from_json("../config_files/simple_generation_config.json")
+    simple_config = TextGenerationConfig.from_yaml("./config/simple_generation_config.yml")
+    print("Model name: {}".format(simple_config.model_name_or_path))
+
+    config_dict = asdict(simple_config)
+    print("Temperature value: {}".format(config_dict["temperature"]))
---
+++ src/vocabulary.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass, asdict
+import json, yaml
+import argparse
+
+"""
+Implementation of the Vocabulary class (Example 3-15) from NLP with Pytorch as a dataclass
+"""
+ # TODO: Figure out how to implement serializable funtions, is it needed?
+
+@dataclass
+class Vocabulary:
+    token_to_idx: dict
+    add_unk: bool
+    unk_token: str
+    
+    def __post_init__(self):
+        """
+        Args:
+            token_to_idx (dict): a pre-existing map of topkens to indicies.
+            add_unk (bool): a flag that indicates whether to add the UNK token.
+            unk_token (str): the UNK token to add into the Vocabulary.
+        """
+        if self.token_to_idx is None:
+            self.token_to_idx = {}
+
+        self.idx_to_token = {idx: token for token, idx in self.token_to_idx.items()}
+
+        self.unk_index = -1
+        if self.add_unk:
+            self.unk_index = self.add_token(self.unk_token)
+
+    def add_token(self, token):
+        """
+        Update maping dicts based on the token.
+
+        Args:
+            token (str): the item to add into the Vocabulary.
+        Returns:
+            index (int): the integer corresponging to the token. 
+        """
+
+        if token in self.token_to_idx:
+            index = self.token_to_idxself.token_to_idx
+        else:
+            index = len(self.token_to_idx)
+            self.token_to_idx[token] = index
+            self.idx_to_token[index] = token
+        return index
+
+    def lookup_token(self, token):
+        """self.token_to_idx
+        Retrieve the index associated with the token or the UNK index if token isn't present.
+        
+        Args:self.token_to_idx
+            token (str): the token to look up.
+        Returns: 
+            index (int): the index corresponding to the token.
+        Note:self.token_to_idx
+            'unk_index' needs to be >=0 (having been added into the vocabulary for the UNK functionality)
+        """
+
+        if self.add_unk:
+            return self.token_to_idx.get(token, self.unk_index)
+        else:
+            return self.token_to_idx[token]
+
+    def lookup_index(self, index):
+        """
+        Retrieve the token associated with the token or the UNK index if token isn't present.
+        
+        Args:
+            token (str): the index to look up.
+        Returns: 
+            index (int): the token corresponding to the index.
+        Raises:
+            Keyerror: if the index is not in the Vocabulary.
+        """
+
+        if index not in self.idx_to_token:
+            raise Keyerror("the index (%d) is not in the Vocabulary" % index)
+        return self.idx_to_token[index]
+
+    def __str__(self):
+        return "<Vocabulary(size=%d)>" % len(self)
+    
+    def __len__(self):
+        return len(self.token_to_idx)
+
+   
+
+
+
+if __name__ == "__main__":
+    tokens = {"How": 1, "are": 2, "you": 3}
+    vocab = Vocabulary(tokens, True, "<UNK>")
+
+    print("Index for token 'how': {}".format(vocab.lookup_token("How")))
+    print("Index for token 'you': {}".format(vocab.lookup_token("you")))
+    print("Token at index '2': {}".format(vocab.lookup_index(2)))
