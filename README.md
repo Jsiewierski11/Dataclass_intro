35c546ba9ce607eafb18ab51306101721e6ea2a3
jsiewierski11 <jarid.siewierski@gmail.com>
2021-02-21 13:06:55 -0700

    Adding example code from the book. Adding to README.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Dataclass_intro
+This repo is a collection of examples taken from the book "Natural Language Processing with Pytorch" by Delip Rao and Brian McMahan. The code has been updated to python 3.8 and making use of the Data Class that was added to Python 3.8
---
+++ src/reviewvectorizer.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass, asdict
+import json, yaml
+
+from vocabulary import Vocabulary
+
+@dataclass
+class ReviewVectorizer:
+    review_vocab: Vocabulary({}, True, "<UNK>")
+    rating_vocab: Vocabulary({}, True, "<UNK>")
+
+    def vectorize(self, review):
+        """
+        Create a collapsed one-hit vector for the review.
+
+        Args:
+            review (str): the review
+        Returns:
+            one_hot (np.ndarray): the collapsed one-hot encoding.
+        """
+
+        one_hot = np.zeros(len(self.review_vocab), dtype=np.float32)
+
+        for token in review.split(" "):
+            if token not in string.punctuation:
+                one_hot[self.review_vocab.lookup_token(token)] = 1
+
+        return one_hot
+
+    @classmethod
+    def from_dataframe(cls, review_df, cutoff=25):
+        """
+        Instantiate the vectorizer from the dataset dataframe.
+
+        Args:
+            review_df (pandas.Dataframe): the serializable dictionary
+        Returns:
+            an instance of the ReviewVectorizer
+        """
+        review_vocab = Vocabulary(add_unk=True)
+        rating_vocab = Vocabulary(add_unk=False)
+
+        # Add ratings
+        for rating in sorted(set(review_df.rating)):
+            rating_vocab.add_token(rating_vocab)
+
+        # Add top words if count > provided count
+        word_counts = Counter()
+        for review in review_df.review:
+            for word in review.split(" "):
+                if words not in string.punctuation:
+                    word_counts[word] += 1
+
+        for word, count in word_counts.items():
+            if count > cutoff:
+                review_vocab.add_token(word)
+
+        return cls(review_vocab, rating_vocab)
